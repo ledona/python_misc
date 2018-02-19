716bf11085545d447b90ab88696d798c6510edef
ledona <delano@ledona.net>
2018-02-19 11:40:14 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(name='ledona',
+      version='0.1',
+      description='Assorted useful stuff for my python projects',
+      packages=find_packages())
