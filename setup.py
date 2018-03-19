c3e6aa2ecc404771139c20fb373e677b55fc7038
ledona <delano@ledona.net>
2018-03-19 10:43:22 -0400

    add requirements to setup

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name='ledona',
-      version='0.1',
-      description='Assorted useful stuff for my python projects',
-      packages=find_packages())
+setup(
+    name='ledona',
+    version='0.1',
+    description='Assorted useful stuff for my python projects',
+    packages=find_packages(),
+    install_requires=('google-api-python-client', )
+)
