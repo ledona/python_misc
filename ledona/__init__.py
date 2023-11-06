b38cd45c48fae21818bc5d0debbdee4b7cb0f61e
ledona <delano@ledona.net>
2023-11-06 20:40:31 +0000

    improved hasher

--- ledona/__init__.py
+++ ledona/__init__.py
@@ -3,6 +3,7 @@ import time
 import argparse
 from datetime import timedelta
 import hashlib
+import pickle
 
 from . import sqlalchemy
 from .base_test_class import BaseTestClass
@@ -18,10 +19,13 @@ from .profiler import profileit
 
 
 def constant_hasher(obj, as_int=True):
-    """return a constant (from one python run to the next) hash value for obj"""
-    hasher = hashlib.sha1()
-    hasher.update(repr(obj).encode("utf-8"))
-    return int(hasher.hexdigest(), 16) if as_int else hasher.hexdigest()
+    """
+    return a constant (from one python run to the next) hash value for obj
+    note that the obj's repr is what is actually hashed
+    """
+    hash_arg = pickle.dumps(obj)
+    hash_val = hashlib.md5(hash_arg)
+    return int.from_bytes(hash_val.digest(), "big") if as_int else hash_val.hexdigest()
 
 
 def process_timer(timed_func):
@@ -55,3 +59,19 @@ class ArgumentDefaultsHelpNoNoneFormatter(argparse.HelpFormatter):
                 if action.option_strings or action.nargs in defaulting_nargs:
                     help_ += " (default: %(default)s)"
         return help_
+
+
+__all__ = [
+    "sqlalchemy",
+    "BaseTestClass",
+    "deep_compare",
+    "deep_compare_dicts",
+    "deep_compare_objs",
+    "compare_dataframes",
+    "deep_compare_ordered_collections",
+    "make_json_compatible",
+    "profileit",
+    "constant_hasher",
+    "process_timer",
+    "ArgumentDefaultsHelpNoNoneFormatter",
+]
